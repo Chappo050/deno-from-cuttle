4913bcb80006695221ed28de2b971f8b4b2ad6d4
Matthew Chaplin <85464120+Chappo050@users.noreply.github.com>
2023-08-16 21:04:00 +1000

    Update webhookRoutes.ts

--- src/routes/webhookRoutes.ts
+++ src/routes/webhookRoutes.ts
@@ -5,6 +5,6 @@ const router = new Router();
 router.prefix('/webhooks');
 
 router
-    .get('/', webhookController.healthCheck);
+    .post('/', webhookController.healthCheck);
 
 export default router;
