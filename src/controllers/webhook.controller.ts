6891c67a40b2b6d66098102f8c6174ead129305f
Matthew Chaplin <85464120+Chappo050@users.noreply.github.com>
2023-08-16 21:05:29 +1000

    Update webhook.controller.ts

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -1,11 +1,28 @@
 // controllers/userController.ts
 
-export const healthCheck = ({ response }: { response: any }) => {
-    response.body = {
-        success: true,
-        data: [
-            { id: 1, name: 'John Doe' },
-            { id: 2, name: 'Jane Doe' },
-        ],
-    };
+export const healthCheck = async ({ request, response }: { request: any, response: any }) => {
+    try {
+        const body = await request.body();
+        const data = body.value;
+
+        // Log the incoming request data
+        console.log("Received webhook data: ", data);
+
+        // Send a response back to the caller
+        response.status = 200;
+        response.body = {
+            success: true,
+            data: "Webhook received successfully"
+        };
+
+    } catch (error) {
+        // Log and handle the error
+        console.error("Error handling webhook: ", error);
+        
+        response.status = 500;
+        response.body = {
+            success: false,
+            message: "Internal Server Error"
+        };
+    }
 };
